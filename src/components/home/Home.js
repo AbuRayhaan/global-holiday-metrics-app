e884163b000e602224e2635100d51e568e2e1982
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-19 15:12:54 +0100

    Added the home page

---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+import HomeCity from './DefaultCity';
+
+const Home = () => {
+  return (
+    <section className="">
+      <HomeCity />
+      <Form />
+    </section>
+  )
+}
+
+export default Home;
