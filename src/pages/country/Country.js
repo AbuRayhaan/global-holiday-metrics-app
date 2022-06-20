9bd64baa0629d19a74d53b8841c374460adc20bd
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-20 13:07:23 +0100

    Added the page dir

---
+++ src/pages/country/Country.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalHoliday from '../../components/holiday/CountryHoliday'
+import HolidayList from '../../components/holiday/HolidayList';
+
+const Country = () => {
+  
+  return (
+    <section className="">
+      <GlobalHoliday />
+      <HolidayList />
+    </section>
+  )
+}
+
+export default Country;
