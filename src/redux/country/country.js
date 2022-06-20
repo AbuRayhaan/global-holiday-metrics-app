e628b0793a97ea122c7b51e346d45b3f6dbfa393
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-20 14:28:23 +0100

    Modified the country to fetch holiday API

--- src/redux/country/country.js
+++ src/redux/country/country.js
@@ -1,6 +1,6 @@
 const initialState = [];
 
-const url = 'https://holidayapi.com/v1/countries?pretty&key=1b53bd42-1ccd-44a8-9d52-4ac475e29c0f';
+const countryAPI = 'https://holidayapi.com/v1/countries?pretty&key=1b53bd42-1ccd-44a8-9d52-4ac475e29c0f';
 
 export const addCountry = (country) => ({
   type: 'ADD_COUNTRY',
@@ -8,7 +8,7 @@ export const addCountry = (country) => ({
 });
 
 export const FetchCountry = () => (async (dispatch) => {
-  const response = await fetch(url);
+  const response = await fetch(countryAPI);
   const info = await response.json();
   const countryArray = [];
   info.countries.forEach((e) => {
@@ -16,10 +16,14 @@ export const FetchCountry = () => (async (dispatch) => {
       countryCode: e.code,
       countryName: e.name,
     };
+    // const code = country.countryCode;
+    // const holidayAPI = `https://holidayapi.com/v1/holidays?pretty&key=1b53bd42-1ccd-44a8-9d52-4ac475e29c0f&country=`&{code}`&year=2021`;
     countryArray.push(country);
+    // console.log(code);
   });
     dispatch(addCountry(countryArray));
 });
+
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
