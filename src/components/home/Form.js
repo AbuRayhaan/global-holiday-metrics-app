1ff506a18f9ad250b301d5c62b335ee9d7a4c0d7
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-20 13:06:57 +0100

    Added the home components

--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { FaExclamationTriangle, FaRegArrowAltCircleRight } from 'react-icons/fa';
+// import { FaExclamationTriangle, FaRegArrowAltCircleRight } from 'react-icons/fa';
 import '../../scss/Form.css'
 const Form = () => (
   <div className="form-container d-flex">
-    <p>SEARCH COUNTRY</p>
+    {/* <p>SEARCH COUNTRY</p> */}
     <form>
-      <input type="number" placeholder="Country"></input>
+      <input type="text" placeholder="Country"></input>
       {/* <input type="number" placeholder="Longitude"></input> */}
       <button type="submit">SEARCH</button> 
     </form>
