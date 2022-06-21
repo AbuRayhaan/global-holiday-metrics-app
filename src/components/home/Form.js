8a7b75c593e4a61c7fa807601b406fb779337714
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-21 16:46:39 +0100

    Modified the Form component

--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-// import { FaExclamationTriangle, FaRegArrowAltCircleRight } from 'react-icons/fa';
+import React from 'react';
 import '../../scss/Form.css'
 
-const Form = () => {
-  const countries = useSelector((state) => state.country);
-  // const [setDisplayedCountries] = useState([]);
-  
-  const searchHandler = (c) => {
-  const country = c.target.value;
-  const list = countries.filter((e) => e.countryName.includes(country));
-  console.log(list);
-  // setDisplayedCountries(list);
-  }
+const Form = ({ onChange }) => {
 
   return (
     <div className="form-container d-flex">
     <form>
-      <input type="text" placeholder="Country" onChange={searchHandler}></input>
+      <input type="text" placeholder="Country" onChange={onChange}></input>
       <button type="submit">SEARCH</button> 
     </form>
   </div>
-  )
+  );
 }
 
 export default Form;
