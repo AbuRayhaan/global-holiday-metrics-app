9fec9c7786c5368a689c342c4bb6ab07c33f772e
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-21 12:31:20 +0100

    Modified the Home components

--- src/components/home/CountryCard.js
+++ src/components/home/CountryCard.js
@@ -5,17 +5,22 @@ import '../../scss/CountryCard.css';
 import { FaRegArrowAltCircleRight } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { FetchCountry } from '../../redux/country/country';
-import { FetchHoliday } from '../../redux/country/holiday';
+// import { FetchHoliday } from '../../redux/country/holiday';
 
 const HomeCity = () => {
   const country = useSelector((state) => state.country);
-
+  // const holiday = useSelector((state) => state.holiday);
+  
   const dispatch = useDispatch();
   
   useEffect(() => {
     dispatch(FetchCountry());
       // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  // useEffect(() => {
+  //   dispatch(FetchHoliday());
+  // })
 
   return (
     <ul className="country-list">
@@ -29,6 +34,7 @@ const HomeCity = () => {
           </NavLink>
           <p className="fa-icons-right">{name}</p>
           <p className="fa-icons-right">{id}</p>
+          {/* <p className="fa-icons-right">{countHoliday(id)}</p> */}
         </li>
       )
       )}
--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 // import { FaExclamationTriangle, FaRegArrowAltCircleRight } from 'react-icons/fa';
 import '../../scss/Form.css'
-const Form = () => (
-  <div className="form-container d-flex">
-    {/* <p>SEARCH COUNTRY</p> */}
+
+const Form = () => {
+  const countries = useSelector((state) => state.country);
+  // const [setDisplayedCountries] = useState([]);
+  
+  const searchHandler = (c) => {
+  const country = c.target.value;
+  const list = countries.filter((e) => e.countryName.includes(country));
+  console.log(list);
+  // setDisplayedCountries(list);
+  }
+
+  return (
+    <div className="form-container d-flex">
     <form>
-      <input type="text" placeholder="Country"></input>
-      {/* <input type="number" placeholder="Longitude"></input> */}
+      <input type="text" placeholder="Country" onChange={searchHandler}></input>
       <button type="submit">SEARCH</button> 
     </form>
   </div>
-)
+  )
+}
 
 export default Form;
