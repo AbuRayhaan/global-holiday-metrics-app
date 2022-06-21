8fefaba520690ed04037b102e84d1389cdc7bd58
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-21 16:47:30 +0100

    Modified the Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Form from '../../components/home/Form';
 import CountryList from '../../components/home/CountryList';
-import GlobalHoliday from '../../components/home/GlobalHoliday'
+import GlobalHoliday from '../../components/home/GlobalHoliday';
 
 const Home = () => {
   
   return (
     <section className="">
       <GlobalHoliday />
-      <Form />
       <CountryList />
     </section>
   )
