5775840df5d8e828a66124e74e83cfd7b4d5cdf5
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-21 16:46:00 +0100

    Modified the CountryCard component

--- src/components/home/CountryCard.js
+++ src/components/home/CountryCard.js
@@ -1,44 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-// import { AiOutlineGlobal } from "react-icons/ai";
 import '../../scss/CountryCard.css';
 import { FaRegArrowAltCircleRight } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { FetchCountry } from '../../redux/country/country';
-// import { FetchHoliday } from '../../redux/country/holiday';
+import Form from '../../components/home/Form';
 
 const HomeCity = () => {
   const country = useSelector((state) => state.country);
-  // const holiday = useSelector((state) => state.holiday);
-  
+  const [displayedCountries, setDisplayedCountries] = useState([]);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(FetchCountry());
       // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // useEffect(() => {
-  //   dispatch(FetchHoliday());
-  // })
+  useEffect(() => {
+    setDisplayedCountries(country);
+  }, [country]);
+
+  const searchHandler = (n) => {
+    const countries = n.target.value;
+    const list = country.filter((e) => e.countryName.includes(countries));
+    console.log(countries);
+    setDisplayedCountries(list);
+  }
 
   return (
-    <ul className="country-list">
-      {country.map((
-        {countryCode: id,
-        countryName: name,}
-      ) => (
-        <li className="city-index" key={id}>
-          <NavLink to={`/${id}`}>
-            <FaRegArrowAltCircleRight className="fa-icons-right" />
-          </NavLink>
-          <p className="fa-icons-right">{name}</p>
-          <p className="fa-icons-right">{id}</p>
-          {/* <p className="fa-icons-right">{countHoliday(id)}</p> */}
-        </li>
-      )
-      )}
-    </ul>
+    <section>
+      <Form onChange={searchHandler}/>
+      <ul className="country-list">
+        {displayedCountries.map((
+          {countryCode: id,
+          countryName: name,}
+        ) => (
+          <li className="city-index" key={id}>
+            <NavLink to={`/${id}`}>
+              <FaRegArrowAltCircleRight className="fa-icons-right" />
+            </NavLink>
+            <p className="fa-icons-right">{name}</p>
+            <p className="fa-icons-right">{id}</p>
+            {/* <p className="fa-icons-right">{countHoliday(id)}</p> */}
+          </li>
+        )
+        )}
+      </ul>
+    </section>
   );
 };
 
