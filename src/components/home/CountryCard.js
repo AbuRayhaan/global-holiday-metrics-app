e2b7b4c608e80cef94eb411e378851eebdf6a7a9
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-22 07:53:48 +0100

    Modified the home dir components

--- src/components/home/CountryCard.js
+++ src/components/home/CountryCard.js
@@ -4,16 +4,15 @@ import '../../scss/CountryCard.css';
 import { FaRegArrowAltCircleRight } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { FetchCountry } from '../../redux/country/country';
-import Form from '../../components/home/Form';
+import Form from './Form';
 
-const HomeCity = () => {
+const CountryTiles = () => {
   const country = useSelector((state) => state.country);
   const [displayedCountries, setDisplayedCountries] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(FetchCountry());
-      // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -23,31 +22,31 @@ const HomeCity = () => {
   const searchHandler = (n) => {
     const countries = n.target.value;
     const list = country.filter((e) => e.countryName.includes(countries));
-    console.log(countries);
     setDisplayedCountries(list);
-  }
+  };
 
   return (
     <section>
-      <Form onChange={searchHandler}/>
+      <Form onChange={searchHandler} />
       <ul className="country-list">
         {displayedCountries.map((
-          {countryCode: id,
-          countryName: name,}
+          {
+            countryCode: id,
+            countryName: name,
+          },
         ) => (
           <li className="city-index" key={id}>
             <NavLink to={`/${id}`}>
               <FaRegArrowAltCircleRight className="fa-icons-right" />
             </NavLink>
-            <p className="fa-icons-right">{name}</p>
-            <p className="fa-icons-right">{id}</p>
+            <p className="country-name">{name}</p>
+            <p className="country-code">{id}</p>
             {/* <p className="fa-icons-right">{countHoliday(id)}</p> */}
           </li>
-        )
-        )}
+        ))}
       </ul>
     </section>
   );
 };
 
-export default HomeCity;
+export default CountryTiles;
--- src/components/home/GlobalHoliday.js
+++ src/components/home/GlobalHoliday.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { AiOutlineGlobal } from "react-icons/ai";
+import { AiOutlineGlobal } from 'react-icons/ai';
 import '../../scss/GlobalHoliday.css';
 
-const GlobalHoliday = () => {
-  return (
-    <div className="country-details">
-      <div className="globe">
-        <AiOutlineGlobal className="globe-icon" size={80}/>
-      </div>
-
-      <div className="project-title">
-        <p>Global Holidays</p>
-      </div>
+const GlobalHoliday = () => (
+  <div className="global-details">
+    <div className="globe">
+      <AiOutlineGlobal className="globe-icon" size={80} />
     </div>
-  )
-};
+
+    <div className="project-title">
+      <p>Global Holidays</p>
+    </div>
+  </div>
+);
 
 export default GlobalHoliday;
