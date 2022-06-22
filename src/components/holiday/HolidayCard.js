8b4d25d4bf41d8944f1cdde5149353076194e2aa
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-22 07:53:26 +0100

    Modified the holiday dir components

--- src/components/holiday/HolidayCard.js
+++ src/components/holiday/HolidayCard.js
@@ -1,37 +1,34 @@
 import React from 'react';
-// import { AiOutlineGlobal } from "react-icons/ai";
 import '../../scss/HolidayCard.css';
 import { useSelector } from 'react-redux';
-import { AiOutlineGlobal } from "react-icons/ai";
+import { AiOutlineGlobal } from 'react-icons/ai';
 
 const Holidays = () => {
   const holiday = useSelector((state) => state.holiday);
   const total = holiday.length;
 
   return (
-    <section className="holiday-container">
+    <section className="holiday-container d-flex">
       <div className="country-details">
         <div className="globe">
-          <AiOutlineGlobal className="globe-icon" size={80}/>
+          <AiOutlineGlobal className="globe-icon" size={80} />
         </div>
 
-        <div className="project-title">
-          <p className="">Total:</p>
+        <div className="holiday-title">
+          <p className="">Total Holidays:</p>
           <p>{total}</p>
         </div>
       </div>
 
-      <ul className="holiday-list">
+      <ul className="holiday-list d-flex">
         {holiday.map((
-          {holidayName,
-          holidayDate}
+          { holidayName, holidayDate },
         ) => (
           <li className="holiday-item" key={holidayName}>
             <p className="holiday-name">{holidayName}</p>
             <p className="holiday-date">{holidayDate}</p>
           </li>
-        )
-        )}
+        ))}
       </ul>
     </section>
   );
