0c5d4b7fc8951c511f10c35f665d8e6ca6e59da0
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-22 07:49:07 +0100

    Modified the index.js file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,12 +13,12 @@ root.render(
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // State: i. https://api.openweathermap.org/geo/1.0/reverse?lat=20&lon=30&limit=5&appid=ab0573603a519765828faf48655343cd
 // Pollution: https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=20&lon=30&appid=ab0573603a519765828faf48655343cd
 
-//Holidays API
+// Holidays API
 // Country: https://holidayapi.com/v1/countries?pretty&key=1b53bd42-1ccd-44a8-9d52-4ac475e29c0f
 // Number: https://holidayapi.com/v1/holidays?pretty&key=1b53bd42-1ccd-44a8-9d52-4ac475e29c0f&country=AL&year=2021
