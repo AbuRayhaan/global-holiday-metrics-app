369c2dc5291e1accf510c0f7fbcb88e80721f663
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-22 07:51:47 +0100

    Modified the home page component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,14 +2,11 @@ import React from 'react';
 import CountryList from '../../components/home/CountryList';
 import GlobalHoliday from '../../components/home/GlobalHoliday';
 
-const Home = () => {
-  
-  return (
-    <section className="">
-      <GlobalHoliday />
-      <CountryList />
-    </section>
-  )
-}
+const Home = () => (
+  <section className="">
+    <GlobalHoliday />
+    <CountryList />
+  </section>
+);
 
 export default Home;
