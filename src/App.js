a226f75a5e44437e420acd1b17643cd837a4f7d4
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-22 07:48:43 +0100

    Styled the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Header from "./components/Header";
+import Header from './components/Header';
 import Home from './pages/home/Home';
 import Country from './pages/country/Country';
 import Footer from './components/Footer';
+import './scss/App.css';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />} />
           <Route path="/:country_code" element={<Country />} />
         </Routes>
       </main>
